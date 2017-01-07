6bde01a3b8283f5702b28bf735d5f7a1b357a5bc
DFenniak <donald@getjobber.com>
2017-01-07 16:01:47 -0700

    apply income every month

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -9,20 +9,45 @@ export default Ember.Controller.extend({
   // indicators
   cash: null,
   income: null,
-  debt: null,
-  assets: null,
-  resilience: null,
-  environmental: null,
+  debt: 0,
+  assets: 0,
+  resilience: 0,
+  environment: 0,
 
   actions: {
-    answerQuestion(answer) {
-      this.setAnswer(answer)
-      this.setImpact
+    answerQuestion(impact) {
+      console.log(impact)
+      this.setAnswer(impact)
+
+      this.setImpact(impact)
+      this.applyIncome()
 
       var nextMonth = this.get('month') + 1
       this.set('month', nextMonth)
     }
   },
+
+  applyIncome: function() {
+    var income = parseInt(this.get('income'))
+    var currentCash = parseInt(this.get('cash'))
+    this.set('cash', currentCash + income)
+  },
+
+  setImpact: function(impact) {
+    if (impact.cash != undefined) {
+      this.set('cash', parseInt(this.get('cash')) + impact.cash)
+    }
+    if (impact.resilience != undefined) {
+      this.set('resilience', this.get('resilience') + impact.resilience)
+    }
+    if (impact.income != undefined) {
+      this.set('income', parseInt(this.get('income')) + impact.income)
+    }
+    if (impact.environment != undefined) {
+      this.set('environment', this.get('environment') + impact.environment)
+    }
+  },
+
   setAnswer: function(answer) {
     var answers = this.get('answers')
 
