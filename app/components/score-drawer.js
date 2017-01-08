a312610d00488fbf9d60e1e4549067de00b8443d
DFenniak <donald@getjobber.com>
2017-01-08 11:39:08 -0700

    add month name

--- app/components/score-drawer.js
+++ app/components/score-drawer.js
@@ -4,4 +4,39 @@ export default Ember.Component.extend({
   character: null,
   month: null,
   answers: null,
+  monthName: Ember.computed('month', 'character', function() {
+    if (this.get('character') == 'Lamisi') {
+      return this.get('lamisiMonths')[this.get('month') - 1]
+    } else {
+      return this.get('normalMonths')[this.get('month') - 1]
+    }
+  }),
+  lamisiMonths: [
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+    'January',
+    'February',
+    'March'
+  ],
+  normalMonths: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
 });
