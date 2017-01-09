d67a686c25f0bb6a23a0b8b19454233ef4cd7fd4
DFenniak <donald@getjobber.com>
2017-01-08 18:57:49 -0700

    best guesses at cash indicators

--- app/components/game-over.js
+++ app/components/game-over.js
@@ -1,4 +1,51 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  cashLow: function() {
+    var character = this.get('character')
+
+    if (character == 'Zara') {
+      return this.get('cash') <= 8000
+    };
+
+    if (character == 'Lamisi') {
+      return this.get('cash') <= 7000
+    };
+
+    if (character == 'Kojo') {
+      return this.get('cash') <= 4000
+    };
+  },
+
+  cashMedium: function() {
+    var character = this.get('character')
+
+    if (character == 'Zara') {
+      return ((this.get('cash') < 13000) && (this.get('cash') > 8000))
+    };
+
+    if (character == 'Lamisi') {
+      return ((this.get('cash') > 7000 ) && ((this.get('cash') < 10000 )))
+    };
+
+    if (character == 'Kojo') {
+      return ((this.get('cash') > 4000 ) && ((this.get('cash') < 7000 )))
+    };
+  },
+
+  cashHigh: function() {
+    var character = this.get('character')
+
+    if (character == 'Zara') {
+      return this.get('cash') >= 13000
+    };
+
+    if (character == 'Lamisi') {
+      return this.get('cash') >= 10000
+    };
+
+    if (character == 'Kojo') {
+      return this.get('cash') >= 7000
+    };
+  }
 });
